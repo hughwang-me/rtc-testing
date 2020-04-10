131b6ab7bedfdeb7025e95a0ca771cf724e4d56c
Hugh Wang <18501667737@163.com>
2020-04-10 09:02:57 +0800

    by bash

---
+++ src/main/java/com/uwjx/rtctesting/RtcTestingApplication.java
@@ -0,0 +1,17 @@
+package com.uwjx.rtctesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class RtcTestingApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtcTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/rtctesting/RtcTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.rtctesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
